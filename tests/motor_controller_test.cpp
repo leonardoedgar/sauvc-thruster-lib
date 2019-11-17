9de6c55d5868dc1703e37734179542afc313a56c
leonardoedgar <leonardo_edgar98@outlook.com>
2019-11-17 18:51:31 +0800

    Refactored test file using fixtures

--- tests/motor_controller_test.cpp
+++ tests/motor_controller_test.cpp
@@ -3,19 +3,21 @@
 # include "motor_controller.h"
 #include <vector>
 
-TEST(MotorController, correct_initialisation) {
+class MotorControllerTest:public::testing::Test {
+protected:
     int no_of_motors_to_register {8};
     MotorController sample_controller {no_of_motors_to_register};
+};
+
+TEST_F(MotorControllerTest, correct_initialisation) {
     ASSERT_EQ(int(sample_controller.motors.size()), no_of_motors_to_register);
 }
 
-TEST(MotorController, motion_to_motors_correctly_mapped) {
-    int no_of_motors_to_register {8};
-    MotorController sample_controller {no_of_motors_to_register};
+TEST_F(MotorControllerTest, motion_to_motors_correctly_mapped) {
     std::vector<int> motor_ids_to_run_on_forward {1, 2, 3, 4};
     ASSERT_EQ(sample_controller.motor_ids_for_motion["forward"], motor_ids_to_run_on_forward);
 }
 
-TEST(MotorController, move_forward_with_correct_speed) {
+TEST_F(MotorControllerTest, move_forward_with_correct_motors_and_speed) {
     ASSERT_EQ(true, true);
 }
