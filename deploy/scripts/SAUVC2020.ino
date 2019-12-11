4eb2331112ad595ab855467aefb7c8b768b36358
leonardoedgar <leonardo_edgar98@outlook.com>
2019-12-11 22:41:03 +0800

    minor bugfix in reading depth sensor

--- deploy/scripts/SAUVC2020.ino
+++ deploy/scripts/SAUVC2020.ino
@@ -10,35 +10,35 @@ MotorController motor_controller {motor_pins};
 int e_stop_pin = 7;
 
 MS5837 depth_sensor;
-float operating_depth = 1.5, depth_tolerance = 0.25, current_depth = 0;
+float operating_depth = -1, depth_tolerance = 0.25, current_depth = 0;
 
 void setup() {
   // Delay 1 second to finish setting up all motor pins.
   delay(1000);
   
-  pinMode(e_stop_pin, INPUT);
-  
-  Serial.begin(9600);
-  
-  Wire.begin();
+//   pinMode(e_stop_pin, INPUT);
+   
+   Serial.begin(9600);
+   
+//   Wire.begin();
 
   // Initialize pressure sensor
   // Returns true if initialization was successful
   // We can't continue with the rest of the program unless we can initialize the sensor
-  while (!depth_sensor.init()) {
-    Serial.println("Init failed!");
-    Serial.println("Are SDA/SCL connected correctly?");
-    Serial.println("Blue Robotics Bar30: White=SDA, Green=SCL");
-    Serial.println("\n\n\n");
-    delay(5000);
-  }
+//  while (!depth_sensor.init()) {
+//    Serial.println("Init failed!");
+//    Serial.println("Are SDA/SCL connected correctly?");
+//    Serial.println("Blue Robotics Bar30: White=SDA, Green=SCL");
+//    Serial.println("\n\n\n");
+//    delay(5000);
+//  }
   depth_sensor.setModel(MS5837::MS5837_30BA);
   depth_sensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)
 }
 
 void loop() {
   // Code to test thrusters' functionality.
-
+  Serial.println("main loop");
 //  motor_controller.move("forward", 75);
 //  delay(1000);
 //  motor_controller.stop();
@@ -50,28 +50,28 @@ void loop() {
 
     // Code to test e-stop's functionality.
 
-//    int val = digitalRead(e_stop_pin);
-//    Serial.println(val);
-//    if (is_estop_pressed()) {
-//      Serial.println("Pressed.");
-//    }
-//    else {
-//      Serial.println("Not pressed.");
-//    }
-//    delay(500);
-
-    // Code to test depth sensor's functionality.
-
-    current_depth = get_current_depth();
-    if (operating_depth + depth_tolerance < current_depth) {
-      Serial.println("Motor go up"); 
-    }
-    else if (operating_depth-depth_tolerance > current_depth) {
-      Serial.println("Motor go down");
+    int val = digitalRead(e_stop_pin);
+    Serial.println(val);
+    if (is_estop_pressed()) {
+      Serial.println("Pressed.");
     }
     else {
-      Serial.println("Stop vertical motion");
+      Serial.println("Not pressed.");
     }
+    delay(500);
+
+    // Code to test depth sensor's functionality.
+//
+//    current_depth = get_current_depth();
+//    if (operating_depth + depth_tolerance < current_depth) {
+//      Serial.println("Motor go up"); 
+//    }
+//    else if (operating_depth-depth_tolerance > current_depth) {
+//      Serial.println("Motor go down");
+//    }
+//    else {
+//      Serial.println("Do nothing");
+//    }
     delay(500);
 }
 
