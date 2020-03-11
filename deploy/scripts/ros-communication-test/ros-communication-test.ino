d971b50d3f3a8d504d7211d864cac42ffa20aac0
quan <quanng95@gmail.com>
2020-03-11 18:01:34 +0800

    minor change in test script

--- deploy/scripts/ros-communication-test/ros-communication-test.ino
+++ deploy/scripts/ros-communication-test/ros-communication-test.ino
@@ -7,14 +7,26 @@ sauvc_msgs::MotionData motion_data;
 
 void messageCb(sauvc_msgs::ThrustersSpeed& msg){
   motion_data.motion = "forward";
-  motion_data.thrusters_speed.thruster_id1_speed = int(msg.thruster_id1_speed) + 1;
-  motion_data.thrusters_speed.thruster_id2_speed = int(msg.thruster_id2_speed) + 1;
-  motion_data.thrusters_speed.thruster_id3_speed = int(msg.thruster_id3_speed) + 1;
-  motion_data.thrusters_speed.thruster_id4_speed = int(msg.thruster_id4_speed) + 1;
-  motion_data.thrusters_speed.thruster_id5_speed = int(msg.thruster_id5_speed) + 1;
-  motion_data.thrusters_speed.thruster_id6_speed = int(msg.thruster_id6_speed) + 1;
-  motion_data.thrusters_speed.thruster_id7_speed = int(msg.thruster_id7_speed) + 1;
-  motion_data.thrusters_speed.thruster_id8_speed = int(msg.thruster_id8_speed) + 1;
+  if (int(msg.thruster_id1_speed) > 1700 ) {
+    msg.thruster_id1_speed = 1700;
+  }
+  else if (int(msg.thruster_id1_speed) < 1300) {
+    msg.thruster_id1_speed = 1300;
+  }
+  if (int(msg.thruster_id2_speed) > 1700 ) {
+    msg.thruster_id2_speed = 1700;
+  }
+  else if (int(msg.thruster_id2_speed) < 1300) {
+    msg.thruster_id2_speed = 1300;
+  }
+  motion_data.thrusters_speed.thruster_id1_speed = int(msg.thruster_id1_speed);
+  motion_data.thrusters_speed.thruster_id2_speed = int(msg.thruster_id2_speed);
+  motion_data.thrusters_speed.thruster_id3_speed = int(msg.thruster_id3_speed);
+  motion_data.thrusters_speed.thruster_id4_speed = int(msg.thruster_id4_speed);
+  motion_data.thrusters_speed.thruster_id5_speed = int(msg.thruster_id5_speed);
+  motion_data.thrusters_speed.thruster_id6_speed = int(msg.thruster_id6_speed);
+  motion_data.thrusters_speed.thruster_id7_speed = int(msg.thruster_id7_speed);
+  motion_data.thrusters_speed.thruster_id8_speed = int(msg.thruster_id8_speed);
 }
  
 ros::Subscriber<sauvc_msgs::ThrustersSpeed> sub("/thrusters/stabilised_speed", &messageCb );
